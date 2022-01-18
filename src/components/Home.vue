d6ad11de8cd9ed9871601067bea75237f68d6a6a
Abraham Peter <theclassical001@gmail.com>
2022-01-18 18:11:48 +0100

    side navbar completed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,11 +6,40 @@
               <div class="p-6">
                   <img src="01.png" alt="" class="h-10" style="">
               </div>
-              <div class="mx-2">
-                  <button :key="page.id" v-for="page in sideBtn" @click="setID = page.id" :class="`w-full font-xs rounded px-3 py-2 flex items-center justify-start ${setID === page.id ? 'bg-light text-white' :'text-lightest'}`">
-                      <i class="material-icons"> {{page.icon}}</i>
+              <div class="mx-2 mb-5">
+                  <button :key="page.id" v-for="page in sideBtn" @click="setID = page.id" :class="`w-full focus:outline-none text-sm font-semibold rounded px-3 py-2 flex items-center justify-start ${setID === page.id ? 'bg-light text-white' :'text-lightest'}`">
+                      <i class="material-icons mr-3"> {{page.icon}}</i>
                       <p>{{ page.name }}</p>
                   </button>
+              </div>
+              <div class="mx-5">
+                  <h1 class="mb-3 text-xs text-lightest tracking-widest uppercase">Playlists</h1>
+                  <button class="flex items-center justify-start opacity-75 hover:opacity-100 mb-2">
+                      <img src="02.png" class="h-8 w-8 mr-3"/>
+                      <p class="text-sm text-white font-semibold">Create Playlist</p>
+                  </button>
+                  <button class="flex items-center justify-start opacity-75 hover:opacity-100">
+                      <img src="03.png" class="h-8 w-8 mr-3"/>
+                      <p class="text-sm text-white font-semibold">Liked Songs</p>
+                  </button>
+                  <div class="h-px w-full bg-light my-3"></div>
+              </div>
+              <div class="mx-5">
+                  <div class="w-full h-12 overflow-y-scroll">
+                      <p :key="album" v-for="album in albums" class="text-lightest hover:text-white text-sm py-1 font-semibold">{{ album.name }}</p>
+                  </div>
+                  <button class="flex items-center justify-start text-lightest hover:text-white py-2">
+                      <i class="material-icons mr-3 rounded-full border text-sm border-lightest">arrow_downward</i>
+                      <p class="text-sm font-semibold">Install App</p>
+                  </button>
+              </div>
+              <div class="relative pt-4">
+                  <div class="w-full h-full flex justify-end items-start opacity-0 hover:opacity-100 p-2 absolute">
+                      <div class="bg-black rounded-full h-6 w-6">
+                          <i class="material-icons text-white">keyboard_arrow_down</i>
+                      </div>
+                  </div>
+                  <img src="04.jpeg" alt="">
               </div>
           </div>
           <!-- main content -->
@@ -37,7 +66,14 @@ export default {
             {id: 'search', name: 'Search', icon: 'search'},
             {id: 'library', name: 'Your Library', icon: 'bar_chart'},
         ],
-        setID: 'home'
+        setID: 'home',
+        albums: [
+            {name: 'Hello'},
+            {name: 'say something'},
+            {name: 'You are the reason'},
+            {name: 'Someone i loved'},
+            {name: 'Tomorrow'},
+        ],
     };
   },
   methods:{
