3dbad8fbe729a17635c433fbb68c22c42e26d46e
Abraham Peter <theclassical001@gmail.com>
2022-01-18 15:14:18 +0100

    tailwind.config.js updated with fontfamily and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: 'Proxima Nova',
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        'green': '#1DB954',
+        'dark': '#121212',
+        'light': '#282828',
+        'lightest': 'B3B3B3',
+        'darkest': '#191414'
+      }
+    },
   },
   variants: {
     extend: {},
